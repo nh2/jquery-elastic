bc272992934634a1e6880f15ac4f241ea1a4e597
jan.jarfalk <jan.jarfalk@14aa3af0-b55e-11dd-a7db-b583d5cdf5c2>
2011-03-03 17:11:52 +0000

    git-svn-id: http://jquery-elastic.googlecode.com/svn/trunk@27 14aa3af0-b55e-11dd-a7db-b583d5cdf5c2

--- jquery.elastic.source.js
+++ jquery.elastic.source.js
@@ -62,7 +62,7 @@
 					curratedHeight = Math.floor(parseInt(height,10));
 					if($textarea.height() != curratedHeight){
 						$textarea.css({'height': curratedHeight + 'px','overflow':overflow});
-						
+						$textarea.trigger('resize');
 					}
 				}
 				
